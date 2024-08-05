ac3ba33c013beb51acf27025a0cb54f55f510a3e
Amani Bobo <amanibobo1@gmail.com>
2024-08-04 20:27:59 -0400

    bruh

--- components/ui/NavWidthWrapper.tsx
+++ components/ui/NavWidthWrapper.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 
-export default function NavWidthWrapper({ className, children }) {
+export default function NavWidthWrapper({ children }) {
   return (
         <div className={cn("mx-auto w-full max-w-screen px-2.5 md:px-80")}>
           {children}
