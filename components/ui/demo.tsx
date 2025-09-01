ae8fc0c5e1e61e00ac1c972c9354dd1679a87ef9
amanibobo <amanibobo1@gmail.com>
2025-09-01 00:23:05 -0400

    Add video player integration and language-specific templates

--- components/ui/demo.tsx
+++ components/ui/demo.tsx
@@ -1,7 +1,7 @@
 import VideoPlayer from "@/components/ui/video-player"
 
 const VideoPlayerDemo = () => {
-    return <VideoPlayer src="/videooo.mov"/>
+    return <VideoPlayer src="/video2.mov"/>
 }
 
 export { VideoPlayerDemo }
